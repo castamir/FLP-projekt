14d501450ca89b5c49e10da5f1b0b1671bc1d8fa
msurovic <msurovic@users.noreply.github.com>
2014-04-14 03:04:16 +0200

    Update DFA.hs

--- DFA.hs
+++ DFA.hs
@@ -3,7 +3,8 @@ module DFA
 --, nfa2dfa
 )where
 
-import qualified Data.Set as Set
+import qualified Data.Set  as Set
+import qualified Data.List as List
 import NFA
 
 type State      = String
@@ -36,62 +37,101 @@ nfaEpsClosure :: NFA -> NFA
 nfaEpsClosure nfa = NFA { NFA.name   = NFA.name nfa
                         , NFA.states = NFA.states nfa
                         , NFA.alph   = NFA.alph nfa
-                        , NFA.rules  = Set.union (NFA.rules nfa) $ Set.map (\(p,q) -> (p,Nothing,q)) $ transClosure $ reflxClosure epsTransition
+                        , NFA.rules  = Set.union (NFA.rules nfa) $ Set.map (\(p,q) -> (p,Set.empty,q)) $ transClosure epsRelation
                         , NFA.start  = NFA.start nfa
                         , NFA.finish = NFA.finish nfa
                         }
 
-    where epsTransition = Set.fromList [(p,q) | (p,a,q) <- Set.toList (NFA.rules nfa), a == Nothing]
+    where epsRelation = Set.union epsLoop epsRule
+          epsLoop = Set.map (\s -> (s,s)) $ NFA.states nfa
+          epsRule = Set.fromList [(p,q) | (p,a,q) <- Set.toList (NFA.rules nfa), Set.null a]
 
 epsClosureState :: NFA -> State -> Set.Set State
-epsClosureState nfa s = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> p == s && a == Nothing) (NFA.rules nfa)
+epsClosureState nfa s = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> p == s && Set.null a) (NFA.rules nfa)
 
 epsClosureSet :: NFA -> Set.Set State -> Set.Set State
 epsClosureSet nfa s = Set.foldl Set.union Set.empty $ Set.map (epsClosureState nfa) s
 
 deltaSupState :: NFA -> SuperState -> Char -> SuperState
-deltaSupState nfa s x = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> Set.member p s && Set.member x a) $ Set.filter (\(_,a,_) -> a /= Nothing) (NFA.rules nfa)
+deltaSupState nfa s x = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> Set.member p s && Set.member x a) (NFA.rules nfa)
 
 genStatesStep :: NFA -> SuperState -> Set.Set SuperState
 genStatesStep nfa ss = Set.map (epsClosureSet nfa) $ Set.map (deltaSupState nfa ss) (NFA.alph nfa)
 
 genStates :: NFA -> Set.Set SuperState -> Set.Set SuperState
-genStates nfa ss = Set.foldl Set.union ss $ Set.map (genStatesStep nfa) ss
+genStates nfa ss = Set.foldr Set.union ss $ Set.map (genStatesStep nfa) ss
+
+genStatesAll :: NFA -> Set.Set SuperState -> Set.Set SuperState
+genStatesAll nfa ss = if ss == new_ss
+                        then ss
+                        else genStatesAll nfa new_ss
+
+    where new_ss = Set.union ss $ genStates nfa ss
+
+genRulesStep :: NFA -> SuperState -> Set.Set Rule
+genRulesStep nfa ss =  Set.fromList $ zipWith (\(p,_,q) s -> (p,s,q)) groupedRules $ map (Set.fromList . rules2symbols) groupedRules
+    
+    where newRules      = Set.map (\x -> (ss, x, deltaSupState nfa ss x)) (NFA.alph nfa)
+          groupedRules  = List.groupBy (\(_,_,q1) (_,_,q2) -> q1 == q2) $ Set.toList newRules
+          rules2symbols = map (\(_,a,_) -> a)
+
+genRules :: NFA -> Set.Set SuperState -> Set.Set Rule
+genRules nfa ss = Set.foldr Set.union Set.empty $ Set.map (genRulesStep nfa) ss
 
 --nfa2dfa :: NFA -> DFA
---nfa2dfa  nfa = nfa2dfa' $ nfaEpsClosure nfa
---nfa2dfa' nfa = DFA { DFA.name   = NFA.name nfa
---                   , DFA.start  = newStart
---                   , DFA.states = newStates
---                   }
+--nfa2dfa nfa = DFA { DFA.name   = NFA.name nfa
+--                  , DFA.start  = newStart
+--                  , DFA.states = newStates
+--                  , DFA.rules  = newRules
+--                  , DFA.alph   = NFA.alph nfa
+--                  , DFA.finish = newFinish
+--                  } $ nfaEpsClosure nfa
 
---    where epsClosureState s = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> p == s && a == Nothing) (NFA.rules nfa)
---          epsClosureSet   s = Set.foldl Set.union Set.empty $ Set.map epsClosureState s
---          deltaSupState s x = Set.map (\(p,a,q) -> q) $ Set.filter (\(p,a,q) -> Set.member p s && a == x) (NFA.rules nfa)
-
---          genStates s = if s == genStates'
---                          then s
---                          else genStates genStates'
-              
---              where genStates' = Set.union s $ 
-          
---          newStart  = epsClosureState NFA.start nfa
---          newStates = genStates (Set.singleton newStart)
+--    where newStart  = epsClosureState NFA.start nfa
+--          newStates = genStatesAll (Set.singleton newStart)
+--          newRules  = genRules newStates
+--          newFinish = Set.filter (notDisjoint $ NFA.finish nfa) newStates
 
 test_rel = Set.fromList [(1,2),(2,3),(3,4)]
 
-test_nfa_a = NFA { NFA.name   = "a"
-                 , NFA.states = Set.fromList ["a1", "a2"]
-                 , NFA.alph   = Set.singleton 'a'
-                 , NFA.rules  = Set.fromList [("a1", Just (Set.singleton 'a'), "a2")]
-                 , NFA.start  = "a1"
-                 , NFA.finish = Set.fromList ["a2"]
-                 }
+test_nfa_a1 = NFA { NFA.name   = "a"
+                  , NFA.states = Set.fromList ["a11", "a12"]
+                  , NFA.alph   = Set.singleton 'a'
+                  , NFA.rules  = Set.fromList [("a11", Set.singleton 'a', "a12")]
+                  , NFA.start  = "a11"
+                  , NFA.finish = Set.fromList ["a12"]
+                  }
 
-test_nfa_b = NFA { NFA.name   = "b"
-                 , NFA.states = Set.fromList ["b1", "b2"]
-                 , NFA.alph   = Set.singleton 'b'
-                 , NFA.rules  = Set.fromList [("b1", Just (Set.singleton 'b'), "b2")]
-                 , NFA.start  = "b1"
-                 , NFA.finish = Set.fromList ["b2"]
-                 }
+test_nfa_a2 = NFA { NFA.name   = "a"
+                  , NFA.states = Set.fromList ["a21", "a22"]
+                  , NFA.alph   = Set.singleton 'a'
+                  , NFA.rules  = Set.fromList [("a21", Set.singleton 'a', "a22")]
+                  , NFA.start  = "a21"
+                  , NFA.finish = Set.fromList ["a22"]
+                  }                  
+
+test_nfa_b1 = NFA { NFA.name   = "b"
+                  , NFA.states = Set.fromList ["b11", "b12"]
+                  , NFA.alph   = Set.singleton 'b'
+                  , NFA.rules  = Set.fromList [("b11", Set.singleton 'b', "b12")]
+                  , NFA.start  = "b11"
+                  , NFA.finish = Set.fromList ["b12"]
+                  }
+
+test_nfa_b2 = NFA { NFA.name   = "b"
+                  , NFA.states = Set.fromList ["b21", "b22"]
+                  , NFA.alph   = Set.singleton 'b'
+                  , NFA.rules  = Set.fromList [("b21", Set.singleton 'b', "b22")]
+                  , NFA.start  = "b21"
+                  , NFA.finish = Set.fromList ["b22"]
+                  }
+
+test_nfa_b3 = NFA { NFA.name   = "b"
+                  , NFA.states = Set.fromList ["b31", "b32"]
+                  , NFA.alph   = Set.singleton 'b'
+                  , NFA.rules  = Set.fromList [("b31", Set.singleton 'b', "b32")]
+                  , NFA.start  = "b31"
+                  , NFA.finish = Set.fromList ["b32"]
+                  }
+
+test_nfa = NFA.concat (NFA.iter $ NFA.union test_nfa_a1 test_nfa_b1) $ NFA.concat test_nfa_a2 $ NFA.concat test_nfa_b2 test_nfa_b3
