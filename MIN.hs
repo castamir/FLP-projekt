528957acae3d5eb3d58a1d0f50b95d0abe2303bd
msurovic <msurovic@users.noreply.github.com>
2014-04-17 00:25:51 +0200

    Update MIN.hs

--- MIN.hs
+++ MIN.hs
@@ -1,7 +1,3 @@
-module MIN
-(
-) where
-
 import qualified Data.Set as Set
 import qualified Data.List as List
 import DFA
@@ -10,28 +6,25 @@ type State      = String
 type SuperState = Set.Set State
 type Transition = Set.Set Char
 type Rule       = (SuperState, Transition, SuperState)
-type Rules 		= Set.Set Rule
 type SimpleRule = (SuperState, Char, SuperState)
 
-simpleRulesStep :: Rule -> [SimpleRule]
-simpleRulesStep (p,a,q) =  map (\x -> (p,x,q)) $ Set.toList a
-
-simpleRules :: Rules -> [SimpleRule]
-simpleRules rls = concat $ map (simpleRulesStep) $ Set.toList rls
+makeSimpleRules :: Set.Set Rule -> [SimpleRule]
+makeSimpleRules rls = concat $ map (simpleRulesStep) $ Set.toList rls
+    where
+      simpleRulesStep (p,a,q) = map (\x -> (p,x,q)) $ Set.toList a
 
 firstClass :: [SimpleRule] -> Set.Set SuperState -> [(Int, [SimpleRule])]
 firstClass rls finish = [(1, fst x), (2, snd x)]  
-	where 
-		x = List.partition (\(p,_,_) -> Set.member p finish) rls
+    where
+      x = List.partition (\(p,_,_) -> Set.member p finish) rls 
 
-newClass :: [(Int, [SimpleRule])] -> [(Int, [SimpleRule])]
-newClass cls = cls 
+classDelta :: (Int, [SimpleRule]) -> Char -> [SuperState]
+classDelta cls sym = map (\(_,_,q) -> q) $ filter (\(p,a,q) -> a == sym) $ snd cls
 
---newSingleClass :: (Int, [SimpleRule]) -> [(Int, [SimpleRule])] -> [(Int, SuperState)]
---newSingleClass cls classes = 
-	--where
-		--clsCol = filter (\(_,_,q) -> q) $ snd cls 
-		--clsStates = map () clsCol
+classId :: [(Int, [SimpleRule])] -> SuperState -> Int
+classId classes s = fst $ head $ filter (\x -> elem s $ snd x) $ map (\(i,x) -> (i, srcStates x)) classes
+    where
+      srcStates rls = map (\(p,_,_) -> p) rls
 
-findClassId :: SuperState -> [(Int, [SimpleRule])] -> Int
-findClassId a classes = List.find (\(x,y) -> elem a y) classes
+splitClass :: (Int, [SimpleRule]) -> [[SimpleRule]]
+splitClass cls = 
